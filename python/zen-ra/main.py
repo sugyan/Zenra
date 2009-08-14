f99e20c74e18f854931ca8c944fdb952b6cb4c94
sugyan <sugi1982@gmail.com>
2009-08-14 20:05:01 +0900

    twitter API周りの処理を作成

--- python/zen-ra/main.py
+++ python/zen-ra/main.py
@@ -1,12 +1,14 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
 from google.appengine.ext.webapp import WSGIApplication
 from google.appengine.ext.webapp.util import run_wsgi_app
 from twitter.bot import TwitterHandler
 
 
 def main():
+    logging.getLogger().setLevel(logging.DEBUG)
     application = WSGIApplication([
             (r'/bot/.*', TwitterHandler),
             ], debug = True)
--- python/zen-ra/twitter/bot.py
+++ python/zen-ra/twitter/bot.py
@@ -1,11 +1,70 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import base64
+import logging
 import yaml
+import urllib
+from django.utils import simplejson
+from google.appengine.api import urlfetch
 from google.appengine.ext.webapp import RequestHandler
 
+
 class TwitterHandler(RequestHandler):
-    def get(self):
+    def __init__(self):
         # config.yamlから設定情報を取得
-        config = yaml.load(open('config.yaml'))
+        #     ---
+        #     bot:
+        #       username: zenra_bot
+        #       password: ********
+        config_data = yaml.load(open('config.yaml'))
+        bot_config = config_data['bot']
+        self.auth_header = {
+            'Authorization' : 'Basic ' + base64.b64encode(
+                '%s:%s' % (bot_config['username'], bot_config['password'])
+                )
+            }
+
+    def get(self):
+        friends   = self.friends()
+        followers = self.followers()
+        for id in followers:
+            if not id in friends:
+                logging.debug(id)
+
+    def friends(self):
+        url  = 'http://twitter.com/friends/ids.json'
+        result = urlfetch.fetch(
+            url     = url,
+            headers = self.auth_header,
+            )
+        logging.debug(result.status_code)
+        logging.debug(result.content)
+        if result.status_code == 200:
+            return simplejson.loads(result.content)
+
+    def followers(self):
+        url  = 'http://twitter.com/followers/ids.json'
+        result = urlfetch.fetch(
+            url     = url,
+            headers = self.auth_header,
+            )
+        logging.debug(result.status_code)
+        logging.debug(result.content)
+        if result.status_code == 200:
+            return simplejson.loads(result.content)
+
+    def update(self, status):
+        url  = 'http://twitter.com/statuses/update.json'
+        data = urllib.urlencode({
+                'status' : status,
+                })
+        result = urlfetch.fetch(
+            url     = url,
+            method  = urlfetch.POST,
+            payload = data,
+            headers = self.auth_header,
+            )
+        logging.debug(result.status_code)
+        logging.debug(result.content)
 
