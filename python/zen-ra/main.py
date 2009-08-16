f26c55b47092f7b75a6f1f3cdc90072ddc043454
sugyan <sugi1982@gmail.com>
2009-08-17 08:28:31 +0900

    説明ページ作成

--- python/zen-ra/main.py
+++ python/zen-ra/main.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import WSGIApplication
+from google.appengine.ext.webapp import RequestHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {}))
 
 
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
     application = WSGIApplication([
+            ('/', MainHandler),
             ], debug = True)
     run_wsgi_app(application)
 
