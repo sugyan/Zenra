c5a1e7ae81c2e9fa3254fe42cd7109eb0e2ef125
sugyan <sugi1982@gmail.com>
2009-08-21 01:33:21 +0900

    zenrize処理を改良

--- python/zen-ra/zenra.py
+++ python/zen-ra/zenra.py
@@ -7,12 +7,14 @@ import yaml
 
 
 class Zenra:
-    def __init__(self):
+    def __init__(self, appid = None):
         # config.yamlから設定情報を取得
         #     ---
         #     appid: ********
-        config_data = yaml.load(open('../config.yaml'))
-        self.appid = config_data['appid']
+        self.appid = appid
+        if not appid:
+            config_data = yaml.load(open('../config.yaml'))
+            self.appid = config_data['appid']
         self.url   = 'http://jlp.yahooapis.jp/DAService/V1/parse'
         
     def zenrize(self, sentence):
@@ -22,24 +24,27 @@ class Zenra:
             }
         params = urllib.urlencode(postdata)
         result = urllib.urlopen(self.url, params)
-
-        morpheme_list = []
-        # DOM解析 形態素と品詞だけを抽出
         tree = ElementTree.XML(result.read())
+
+        # 係り受け解析の結果を利用し、適切な挿入箇所を判定する
+        text = ''
         chunk_list = tree.find('{urn:yahoo:jp:jlp:DAService}Result').find('{urn:yahoo:jp:jlp:DAService}ChunkList')
         for chunk in chunk_list.findall('{urn:yahoo:jp:jlp:DAService}Chunk'):
-            for morphem in chunk.find('{urn:yahoo:jp:jlp:DAService}MorphemList'):
-                morpheme_list.append({
-                        'Surface' : morphem.find('{urn:yahoo:jp:jlp:DAService}Surface').text.encode('utf-8'),
-                        'POS'     : morphem.find('{urn:yahoo:jp:jlp:DAService}POS').text.encode('utf-8'),
-                        })
+            morphem_list = chunk.find('{urn:yahoo:jp:jlp:DAService}MorphemList')
+            # 動詞が含まれている文節の前に「全裸で」を挿入する
+            if u'動詞' in [morphem.find('{urn:yahoo:jp:jlp:DAService}POS').text for morphem in morphem_list]:
+                # 動詞→接続助詞 文節切れて 動詞 と繋がる場合をどうにかしたい
+                # 例：帰ってくる
+                text += u'全裸で'.encode('utf-8')
+            else:
+                # 動詞がなくても「名詞+助動詞する」を捕捉する
+                for i in range(len(morphem_list) - 1):
+                    if u'名詞' == morphem_list[i].find('{urn:yahoo:jp:jlp:DAService}POS').text and u'助動詞する' == morphem_list[i + 1].find('{urn:yahoo:jp:jlp:DAService}Feature').text.split(',')[1]:
+                        text += u'全裸で'.encode('utf-8')
+                        break
 
-        # 末尾から見ていって、動詞を検出したら「全裸で」を挿入
-        for morpheme in reversed(morpheme_list):
-            if morpheme['POS'] == u'動詞'.encode('utf-8'):
-                morpheme_list.insert(morpheme_list.index(morpheme), {
-                        'Surface' : u'全裸で'.encode('utf-8'),
-                        'POS'     : '',
-                        })
+            # 形態素を繋げ直す
+            for morphem in morphem_list:
+                text += morphem.find('{urn:yahoo:jp:jlp:DAService}Surface').text.encode('utf-8')
 
-        return ''.join(morpheme['Surface'] for morpheme in morpheme_list)
+        return text
