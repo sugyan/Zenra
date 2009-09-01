c911749e16458e5196c6faa7a14a154426cb7506
sugyan <sugi1982@gmail.com>
2009-09-02 00:55:16 +0900

    全裸を挿入する条件をより細かく改良

--- python/zen-ra/zenra.py
+++ python/zen-ra/zenra.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+RESULT       = '{urn:yahoo:jp:jlp:DAService}Result'
+CHUNK_LIST   = '{urn:yahoo:jp:jlp:DAService}ChunkList'
+CHUNK        = '{urn:yahoo:jp:jlp:DAService}Chunk'
+MORPHEM_LIST = '{urn:yahoo:jp:jlp:DAService}MorphemList'
+SURFACE      = '{urn:yahoo:jp:jlp:DAService}Surface'
+FEATURE      = '{urn:yahoo:jp:jlp:DAService}Feature'
+VERB = u'動詞'
+NOUN = u'名詞'
+AUXI = u'助動詞'
+CONJ = u'接続助詞'
+SURU = u'助動詞する'
+ZENRA = u'全裸で'.encode('utf-8')
+
 from xml.etree import ElementTree
 import urllib
 import yaml
@@ -26,25 +39,46 @@ class Zenra:
         result = urllib.urlopen(self.url, params)
         tree = ElementTree.XML(result.read())
 
-        # 係り受け解析の結果を利用し、適切な挿入箇所を判定する
+        # 係り受け解析の結果を利用し、適切な箇所に"全裸で"を挿入
         text = ''
-        chunk_list = tree.find('{urn:yahoo:jp:jlp:DAService}Result').find('{urn:yahoo:jp:jlp:DAService}ChunkList')
-        for chunk in chunk_list.findall('{urn:yahoo:jp:jlp:DAService}Chunk'):
-            morphem_list = chunk.find('{urn:yahoo:jp:jlp:DAService}MorphemList')
-            # 動詞が含まれている文節の前に「全裸で」を挿入する
-            if u'動詞' in [morphem.find('{urn:yahoo:jp:jlp:DAService}POS').text for morphem in morphem_list]:
-                # 動詞→接続助詞 文節切れて 動詞 と繋がる場合をどうにかしたい
-                # 例：帰ってくる
-                text += u'全裸で'.encode('utf-8')
-            else:
-                # 動詞がなくても「名詞+助動詞する」を捕捉する
-                for i in range(len(morphem_list) - 1):
-                    if u'名詞' == morphem_list[i].find('{urn:yahoo:jp:jlp:DAService}POS').text and u'助動詞する' == morphem_list[i + 1].find('{urn:yahoo:jp:jlp:DAService}Feature').text.split(',')[1]:
-                        text += u'全裸で'.encode('utf-8')
-                        break
+        chunk_list = []
+        for chunk in tree.find(RESULT).find(CHUNK_LIST).findall(CHUNK):
+            morphem_list = []
+            for morphem in chunk.find(MORPHEM_LIST):
+                morphem_list.append({
+                        'surface' : morphem.find(SURFACE).text.encode('utf-8'),
+                        'feature' : morphem.find(FEATURE).text.split(',')[:2],
+                        })
 
-            # 形態素を繋げ直す
+            # 動詞が含まれている文節の前に挿入する
+            if VERB in [morphem['feature'][0] for morphem in morphem_list]:
+                should_insert = True
+                try:
+                    if (morphem_list[0]['feature'][0] == VERB and
+                        # 動詞で終わる文節に動詞で始まる文節が続いた場合は除外
+                        ((chunk_list[-1][-1]['feature'][0] == VERB) or
+                         # 動詞→接続助詞で終わる文節に動詞で始まる文節が続いた場合は除外
+                         (chunk_list[-1][-1]['feature'][1] == CONJ and
+                          chunk_list[-1][-2]['feature'][0] == VERB) or
+                         # 動詞→助動詞で終わる文節に動詞で始まる文節が続いた場合は除外
+                         (chunk_list[-1][-1]['feature'][0] == AUXI and
+                          chunk_list[-1][-2]['feature'][0] == VERB))
+                        ):
+                        should_insert = False
+                except IndexError:
+                    pass
+                if should_insert:
+                        text += ZENRA
+            # 動詞が含まれていなくても、名詞→助動詞する の組み合わせがあれば挿入する
+            else:
+                for i in range(len(morphem_list) - 1):
+                    if (morphem_list[i]['feature'][0] == NOUN and
+                        morphem_list[i + 1]['feature'][1] == SURU):
+                        text += ZENRA
+
             for morphem in morphem_list:
-                text += morphem.find('{urn:yahoo:jp:jlp:DAService}Surface').text.encode('utf-8')
+                text += morphem['surface']
+
+            chunk_list.append(morphem_list)
 
         return text
