496c126c7eaab43ddd3b35c9292b389b29ac44e6
sugyan <sugi1982@gmail.com>
2009-10-31 03:12:59 +0900

    全裸bot2 1.0.0

--- perl/bot.pl
+++ perl/bot.pl
@@ -2,49 +2,144 @@
 use strict;
 use warnings;
 
-use AnyEvent;
-use AnyEvent::Twitter::Stream;
+use Coro;
+use Coro::AnyEvent;
 use Config::Any::YAML;
-use Encode 'decode_utf8';
+use Date::Parse 'str2time';
+use Encode 'encode_utf8';
+use List::Util 'shuffle';
 use Net::Twitter::Lite;
+use Text::MeCab;
 
-my $config = Config::Any::YAML->load('config.yaml');
-my $twitter = Net::Twitter::Lite->new(%$config);
+my $twitter = do {
+    my $config = Config::Any::YAML->load('config.yaml');
+    Net::Twitter::Lite->new(%$config);
+};
 
 my $cv = AE::cv;
 # 5分毎にfollower/friendsを更新
-# my $follow = AE::timer 0, 300, sub {
-AE::timer 0, 300, sub {
-    eval {
-        my $friends   = $twitter->friends_ids;
-        my $followers = $twitter->followers_ids;
-        # 差分を検出
-        my %ids = map +( $_ => 1 ), @$friends;
-        for my $id (@$followers) {
-            my $result = delete $ids{$id};
-            # 未フォローユーザをフォローする
-            if (!defined $result) {
-                $twitter->create_friend($id);
+async {
+    while (1) {
+        eval {
+            my $friends   = $twitter->friends_ids;
+            my $followers = $twitter->followers_ids;
+            # 差分を検出
+            my %ids = map +( $_ => 1 ), @$friends;
+            for my $id (@$followers) {
+                my $result = delete $ids{$id};
+                # 未フォローユーザをフォローする
+                if (!defined $result) {
+                    $twitter->create_friend($id);
+                }
             }
-        }
-        # 残りはリムーブすべきユーザ
-        for my $id (keys %ids) {
-            $twitter->destroy_friend($id);
-        }
-    };
+            # 残りはリムーブすべきユーザ
+            for my $id (keys %ids) {
+                $twitter->destroy_friend($id);
+            }
+        };
+        Coro::AnyEvent::sleep 300;
+    }
 };
-my $x; $x = AE::timer 0, 2, sub {
-#     my $statuses = $twitter->home_timeline;
-};
+# friends_timelineからランダムに発言を拾って全裸にする
+async {
+    while (1) {
+        my $sleep = 60;
+        eval {
+            my $statuses = $twitter->friends_timeline({ count => 100 });
+            if (@$statuses > 1) {
+                # 最新と最古のstatusの時差を計測、次の更新へのwait時間とする
+                my $oldest = str2time($statuses->[-1]{created_at});
+                my $newest = str2time($statuses->[ 0]{created_at});
+                $sleep = $newest - $oldest;
+            }
+          ZENRIZE:
+            for my $status (shuffle @$statuses) {
+                my $text = encode_utf8 $status->{text};
+                next if $status->{user}{screen_name} eq 'zenra_bot2';
+                next if $status->{user}{protected};
+                next if $text =~ / RT[ :] .* \@ \w+ /xms;
+                next if $text =~ / \#.+ /xms;
 
-# my $listener = AnyEvent::Twitter::Stream->new(
-#     %$config,
-#     method   => "filter",
-#     track    => 'zenra',
-#     on_tweet => sub {
-#         my $tweet = shift;
-#         warn "$tweet->{user}{screen_name}: $tweet->{text}\n";
-#     },
-# );
+                my $zenrized = zenrize_all($text);
+                if ($text ne $zenrized) {
+                    $twitter->update({
+                        status => "\@$status->{user}{screen_name} が全裸で呟いた: $zenrized",
+                        in_reply_to_status_id => $status->{id},
+                    });
+                    last ZENRIZE;
+                }
+            }
+        };
+        Coro::AnyEvent::sleep($sleep);
+    }
+};
+# 独り言
+async {
+    while (1) {
+        eval {
+            $twitter->update('動作テスト中です');
+        };
+        Coro::AnyEvent::sleep(rand 60 * 60 * 3);
+    }
+};
 
 $cv->recv;
+
+# テキスト全体を全裸にする
+sub zenrize_all {
+    my $text = shift;
+
+    my $result = '';
+    for my $sentence (split/(\s+)/, $text) {
+        $result .= $sentence =~ /\s+/ ?
+          $sentence : zenrize($sentence);
+    }
+    return $result;
+}
+
+# 日本語の文章を全裸にする
+sub zenrize {
+    my $sentence = shift;
+
+    my $zenra  = '全裸で';
+    my $mecab  = Text::MeCab->new();
+    my $result = '';
+    my $n = $mecab->parse($sentence);
+
+    # 末尾まで進める
+    $n = $n->next while ($n->next);
+
+    my $flg = 0;
+    # 末尾からさかのぼる
+    while (($n = $n->prev)->prev) {
+        # フラグがたっていれば「全裸で」を挿入
+        # ただし、名詞／副詞／動詞のときはまだ挿入しない
+        if ($flg) {
+            my $insert = 1;
+            if ($n->feature =~ / \A (名詞|副詞|動詞) /xms) {
+                $insert = 0;
+            }
+            # また、連用形の動詞→助(動)詞の場合も挿入しない
+            elsif ($n->feature =~ / \A 助(動)?詞 /xms &&
+                       (split(/,/, $n->prev->feature))[5] =~ / 連用 /xms) {
+                $insert = 0;
+            }
+            if ($insert) {
+                $result = $zenra . $result;
+                $flg = 0;
+            }
+        }
+        # 出力の連結
+        $result = $n->surface . $result;
+        # 動詞を検出してフラグをたてる
+        if ($n->feature =~ / \A 動詞 /xms) {
+            $flg = 1;
+        }
+    }
+    # 先頭のチェック
+    if ($flg) {
+        $result = $zenra . $result;
+    }
+
+    return $result;
+}
