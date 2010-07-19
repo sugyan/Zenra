4d6d0b47cb31bf665e7d0bab15a8283231d0e3c6
sugyan <sugi1982@gmail.com>
2010-07-19 11:25:55 +0900

    諸々修正

--- perl/Zenra/lib/Zenra/Controller/Root.pm
+++ perl/Zenra/lib/Zenra/Controller/Root.pm
@@ -21,6 +21,7 @@ sub end :Private {
     my ($self, $c) = @_;
 
     unless ($c->res->body or $c->res->status =~ /^3\d\d/) {
+        $c->res->content_type('text/html');
         $c->forward($c->view('MT'));
     }
 }
@@ -29,60 +30,60 @@ sub index :Path :Args(0) {
     my ($self, $c) = @_;
 }
 
-sub zenrize :Local {
-    my ($self, $c) = @_;
+# sub zenrize :Local {
+#     my ($self, $c) = @_;
 
-    my $tw_conf = models('conf')->{twitter};
-    my $ntl = Net::Twitter::Lite->new(%{ $tw_conf->{oauth} });
-    $c->redirect($ntl->get_authorization_url(callback => $tw_conf->{callback_url}));
-}
+#     my $tw_conf = models('conf')->{twitter};
+#     my $ntl = Net::Twitter::Lite->new(%{ $tw_conf->{oauth} });
+#     $c->redirect($ntl->get_authorization_url(callback => $tw_conf->{callback_url}));
+# }
 
-sub callback :Local {
-    my ($self, $c) = @_;
+# sub callback :Local {
+#     my ($self, $c) = @_;
 
-    my $token    = $c->req->param('oauth_token')    or $c->detach('/default');
-    my $verifier = $c->req->param('oauth_verifier') or $c->detach('/default');
+#     my $token    = $c->req->param('oauth_token')    or $c->detach('/default');
+#     my $verifier = $c->req->param('oauth_verifier') or $c->detach('/default');
 
-    my $ntl = Net::Twitter::Lite->new(
-        %{ models('conf')->{twitter}{oauth} }
-    );
-    my ($access_token, $access_token_secret, $user_id, $screen_name);
-    my $error;
-    try {
-        ($access_token, $access_token_secret, $user_id, $screen_name) =
-            $ntl->request_access_token(
-                token_secret => '',
-                token        => $token,
-                verifier     => $verifier,
-            );
-        $ntl->access_token($access_token);
-        $ntl->access_token_secret($access_token_secret);
+#     my $ntl = Net::Twitter::Lite->new(
+#         %{ models('conf')->{twitter}{oauth} }
+#     );
+#     my ($access_token, $access_token_secret, $user_id, $screen_name);
+#     my $error;
+#     try {
+#         ($access_token, $access_token_secret, $user_id, $screen_name) =
+#             $ntl->request_access_token(
+#                 token_secret => '',
+#                 token        => $token,
+#                 verifier     => $verifier,
+#             );
+#         $ntl->access_token($access_token);
+#         $ntl->access_token_secret($access_token_secret);
 
-        my $statuses = $ntl->user_timeline({count => 200});
-        for my $status (shuffle @$statuses) {
-            next if $status->{in_reply_to_status_id};
+#         my $statuses = $ntl->user_timeline({count => 200});
+#         for my $status (shuffle @$statuses) {
+#             next if $status->{in_reply_to_status_id};
 
-            my $zenra    = decode_utf8 models('util')->zenra;
-            my $zenrized = decode_utf8 models('util')->zenrize(encode_utf8 $status->{text});
-            next unless ($zenrized =~ /$zenra/);
+#             my $zenra    = decode_utf8 models('util')->zenra;
+#             my $zenrized = decode_utf8 models('util')->zenrize(encode_utf8 $status->{text});
+#             next unless ($zenrized =~ /$zenra/);
 
-            my $text = "\@${screen_name}が以前にも全裸で言ったけど: $zenrized #zenra";
-            next if length($text) > 140;
+#             my $text = "\@${screen_name}が以前にも全裸で言ったけど: $zenrized #zenra";
+#             next if length($text) > 140;
 
-            $ntl->update({
-                status => $text,
-                in_reply_to_status_id => $status->{id},
-            });
+#             $ntl->update({
+#                 status => $text,
+#                 in_reply_to_status_id => $status->{id},
+#             });
 
-            last;
-        }
-    } catch {
-        $error = $_;
-    };
-    $c->detach('/index') if $error;
+#             last;
+#         }
+#     } catch {
+#         $error = $_;
+#     };
+#     $c->detach('/index') if $error;
 
-    $c->redirect("http://twitter.com/$screen_name");
-}
+#     $c->redirect("http://twitter.com/$screen_name");
+# }
 
 __PACKAGE__->meta->make_immutable;
 
--- perl/Zenra/root/index.mt
+++ perl/Zenra/root/index.mt
@@ -1,7 +1,6 @@
 ? extends 'common/base';
 
 ? block content => sub {
-<a href="/zenrize">全裸的な何か</a><br />
 ? if ($c->user) {
 <?= $c->user->obj->screen_name ?> <a href="/logout">ログアウト</a><br />
 ? } else {
