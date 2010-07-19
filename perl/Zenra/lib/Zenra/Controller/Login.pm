60522e13049cd28728b3c47a57ab1bd9b3dd7b3f
sugyan <sugi1982@gmail.com>
2010-07-20 01:22:39 +0900

    ログイン失敗時の処理

--- perl/Zenra/lib/Zenra/Controller/Login.pm
+++ perl/Zenra/lib/Zenra/Controller/Login.pm
@@ -1,13 +1,37 @@
 package Zenra::Controller::Login;
 use Ark 'Controller';
+use Try::Tiny;
 
 sub index :Path :Args(0) {
     my ($self, $c) = @_;
 
-    my $auth = $c->auth->authenticate_twitter(
-        callback => $c->uri_for('/login'),
-    );
+    my $auth;
+    if ($c->req->param('oauth_verifier')) {
+        my $error;
+        try {
+            $auth = $c->auth->authenticate_twitter;
+        } catch {
+            $error = $_;
+        };
+
+        if ($error) {
+            $c->log(error => $error);
+            $c->redirect_and_detach('/login/failed');
+        }
+    }
+    else {
+        $auth = $c->auth->authenticate_twitter(
+            callback => $c->uri_for('/login'),
+        );
+    }
+
     $c->redirect_and_detach('/home') if $auth;
+}
+
+sub failed :Local :Args(0) {
+    my ($self, $c) = @_;
+
+    $c->redirect_and_detach('/') if $c->user;
 }
 
 __PACKAGE__->meta->make_immutable;
---
+++ perl/Zenra/root/login/failed.mt
@@ -0,0 +1,8 @@
+? extends 'common/base';
+
+? block content => sub {
+<h1>エラー</h1>
+ログインに失敗しちゃったっぽいです ＞＜<br />
+もう一度試してみて下さい！<br />
+<a href="/login">OAuthでログインする</a><br />
+? }
